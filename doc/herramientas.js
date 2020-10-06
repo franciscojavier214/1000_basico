fa080de881b320a580aa69e94d757e88ab3ef659
DuranGanforninaFranciscojavier <francsijavierdugan@gmail.com>
2020-10-06 12:17:16 +0200

    first commit

---
+++ doc/herramientas.js
@@ -0,0 +1,46 @@
+
+/*
+    Instalación de Mongodb
+Primero nos dirigimos a la pagina https://www.mongodb.com/download-center/community , hacemos 
+click en ""Download", en lado derecho de la web y esperamos su descarga, cuando este lista abra 
+el documento .msi y ejecutelo, en la siguiente ventada cliquee en "Next" , en la posterior 
+presione "Cmplete" y de nuevo "Next", nos aseguramos de que en la nueva ventana este selecionado
+el primer checkbox y la casilla "Run service as Network Service user",en la penultima venta
+desmarque el checkbox  "Install MongoDB Compass" y cliquee nuevamente "Next", en la ultima 
+ventana ,presione Install.
+Vaya al panel de control, Sistema y seguridad, Sistema, configuración avanzada del sistema 
+(parte superior izquierda), variables de entorno, dentro de variables del sistema , Edite "Path"
+y añada la ruta en la que se instalo Mongodb y pulse aceptar dos veces.
+ 
+Para instalar Compass descargelo en aqui https://www.mongodb.com/try/download/compass, extraigalo
+y siga las indicaciones.
+
+    Funcionamiento de Mongodb: Es una base de datos NoSQL, enfocado al openSource.
+*/
+
+/*
+
+    Instalacion de Git 
+Entre en la siguiente pagina https://git-scm.com/ y presione Download, abra el archivo como 
+administrador y pulse "Next >" 3 veces consecutivas, marque la ultima opción y continue 
+presionandolo, en la ultima, presione "Install".
+
+    Funcionamiento de Git: esa una herramienta que permite contralar las versiones de tus programas
+    y permite trabajar a varios programadores actualizando el codigo de estos.
+*/
+
+/*
+    Instalacion de Visual Studio Code 
+entre en https://visualstudio.microsoft.com/es/ y elija la opción Visual Studio Code, ejecutelo
+y prosiga con la instalación.
+
+    Funcionamiento de Visual Studio Code: es el editor de codigo y te ofrece ayuda en la programación de codigo.
+*/
+
+/*
+    GitHub
+entre en https://github.com/, y registrese, no es un web y no requiere instalaciòn,
+
+    Funcionamiento de GitHub: aloja los proyectos de Git y son accesible mayoritariamente.
+
+*/
---
+++ src/ejercicio001.js
@@ -0,0 +1,2 @@
+db.col001.insertOne({X: "Hola mundo"})
+// este codigo inserta Hola mundo en la colección col001 de mongodb //
